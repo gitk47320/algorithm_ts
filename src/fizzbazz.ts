a233593ae1898659e7be43d54ae98c6a71377e0d
gitk47320 <j.kashi.37204.privateuse@gmail.com>
2022-04-20 22:28:06 +0900

    eslint導入とそのエラー修正

--- src/fizzbazz.ts
+++ src/fizzbazz.ts
@@ -1,17 +1,17 @@
 // fizzbazz
 
 const fizzbazz = () : void => {
-  for(let i = 1 ; i <= 100 ; i++){
-    if(i % 15 === 0){
-      console.log("fizzbazz");
-    }else if(i % 3 === 0){
-      console.log("fizz");
-    }else if(i % 5 === 0){
-      console.log("bazz");
-    }else{
-      console.log(i);
+  for (let i = 1; i <= 100; i++) {
+    if (i % 15 === 0) {
+      console.log('fizzbazz')
+    } else if (i % 3 === 0) {
+      console.log('fizz')
+    } else if (i % 5 === 0) {
+      console.log('bazz')
+    } else {
+      console.log(i)
     }
   }
 }
 
-fizzbazz();
+fizzbazz()
